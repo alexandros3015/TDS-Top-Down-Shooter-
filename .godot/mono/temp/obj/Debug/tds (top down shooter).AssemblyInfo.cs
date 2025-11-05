f43fdb7d0256f6aa0b6fa50c69f576b012c8ab88
alexandros3015 <alexandrosbeachtoys@gmail.com>
2025-11-05 17:28:29 -0500

    Add ShieldSkeleton image asset
    Added ShieldSkeleton.png to Assets/Image and updated Godot import files and cache to include the new texture. This asset can now be used in the project for enemy or player visuals.

--- .godot/mono/temp/obj/Debug/tds (top down shooter).AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/tds (top down shooter).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tds (top down shooter)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0545743a4c8e15476c252a728458ff25bcdb356e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d772ff33a090a42309ced3d53781e2a0a81713c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("tds (top down shooter)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tds (top down shooter)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
